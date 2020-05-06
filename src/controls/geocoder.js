52a684de3d03bf942e6ef0acfde29e4842923c12
tung nguyen thanh <20145117@student.hust.edu.vn>
2020-05-06 15:52:36 +0700

    nothing

--- src/controls/geocoder.js
+++ src/controls/geocoder.js
@@ -82,7 +82,7 @@ export default class Geocoder {
     // Override the control being added to control containers
     if (this.options.container) this.options.position = false;
 
-    this._typeahead = new Typeahead(input, [], {filter: false});
+    this._typeahead = new Typeahead(input, [], {filter: false, limit:10});
     this._typeahead.getItemValue = function (item) {
       return item.place_name;
     };
